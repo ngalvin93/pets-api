56c5f1d9bf475f12a1d2d462c2085f8ba8f7a453
Alvin Ng <ngalvin93@gmail.com>
2019-09-19 19:46:53 -0500

    get requests for owner ids are complete

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.port || 3000;
 
-//app.use()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var owners = [
     {
@@ -35,9 +36,40 @@ var owners = [
     }
 ];
 
+// GET /
 app.get('/', function (request, response, next) {
     response.send('Welcome to my site...')
 })
+
+// GET /api/owners
+app.get('/api/owners', function (request, response, next) {
+    response.send(owners)
+})
+
+// GET /api/owners/:id
+app.get('/api/owners/:id', function (request, response, next) {
+    const singleOwner = owners.find(function (owner) {
+        return owner.id === parseInt(request.params.id)
+    })
+    response.send(singleOwner)
+    next()
+})
+
+// POST /api/owners
+
+// PUT /api/owners/:id
+
+// DELETE /api/owners/:id
+
+// GET /api/owners/:id/pets
+
+// GET /api/owners/:id/pets/:petId
+
+// POST /api/owners/:id/pets
+
+// PUT /api/owners/:id/pets/:petId
+
+// DELETE /api/owners/:id/pets/:petId
 
 app.listen(port, function () {
     console.log(`Server is now listening at port ${port}...`)
