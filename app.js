347e5c793ba579695f4dff9c5cf40dfc097787d1
Alvin Ng <ngalvin93@gmail.com>
2019-09-19 20:25:13 -0500

    progress on requests

--- app.js
+++ app.js
@@ -51,26 +51,82 @@ app.get('/api/owners/:id', function (request, response, next) {
     const singleOwner = owners.find(function (owner) {
         return owner.id === parseInt(request.params.id)
     })
+    if (!singleOwner) {
+        return response.status(404).send('The query you entered is not valid. Please try again.')
+    }
     response.send(singleOwner)
     next()
 })
 
 // POST /api/owners
+app.post('/api/owners', function (request, response, next) {
+
+})
 
 // PUT /api/owners/:id
+app.put('/api/owners/:id', function (request, response, next) {
+    const newOwner = {
+        id: owners.length + 1,
+        name: request.body.name,
+        pets: [
+            {
+                id: 1,
+                name: request.body[2].name,
+                type: request.body[2].type,
+            },
+            {
+                id: 2,
+                name: "Felix",
+                type: "Cat"
+            }
+        ]
+    }
+    
+    const singleOwner = owners.find(function (owner) {
+        return owner.id === parseInt(request.params.id)
+    })
+    if (!singleOwner) {
+        return response.status(404).send('The query you entered is not valid. Please try again.')
+    }
+
+})
 
 // DELETE /api/owners/:id
+app.delete('/api/owners/:id', function (request, response, next) {
+    const singleOwner = owners.find(function (owner) {
+        return owner.id === parseInt(request.params.id)
+    })
+    if (!singleOwner) {
+        return response.status(404).send('The query you entered is not valid. Please try again.')
+    }
+    var index = owners.indexOf(singleOwner)
+    owners.splice(index, 1)
+    response.send(owners)
+})
 
 // GET /api/owners/:id/pets
+app.get('/api/owners/:id/pets', function (request, response, next) {
 
+})
 // GET /api/owners/:id/pets/:petId
-
+app.get('/api/owners/:id/pets/:petId', function (request, response, next) {
+    
+})
 // POST /api/owners/:id/pets
-
+app.post('/api/owners/:id/pets', function (request, response, next) {
+    
+})
 // PUT /api/owners/:id/pets/:petId
-
+app.put('/api/owners/:id/pets/:petId', function (request, response, next) {
+    
+})
 // DELETE /api/owners/:id/pets/:petId
+app.delete('/api/owners/:id/pets/:petId', function (request, response, next) {
+    
+})
 
+
+// LISTEN
 app.listen(port, function () {
     console.log(`Server is now listening at port ${port}...`)
 })
