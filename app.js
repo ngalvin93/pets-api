7daccd8ff2a2b26663c69fff0b234abbbbe6fc62
Alvin Ng <ngalvin93@gmail.com>
2019-09-20 23:15:34 -0500

    completed /api/owners/:id/:petId

--- app.js
+++ app.js
@@ -119,7 +119,20 @@ app.get('/api/owners/:id/pets', function (request, response, next) {
 })
 // GET /api/owners/:id/pets/:petId
 app.get('/api/owners/:id/pets/:petId', function (request, response, next) {
-    
+    // identify the owner from the owners array
+    const singleOwner = owners.find(function (owner) {
+        return owner.id === parseInt(request.params.id)
+    })
+    if (!singleOwner) {
+        return response.status(404).send('The query you entered is not valid. Please try again.')
+    }
+    // from the owner, return the pet with the matching id
+    let singleOwnerIndex = owners.indexOf(singleOwner)
+    let singleOwnerPets = owners[singleOwnerIndex].pets
+    const singlePet = singleOwnerPets.find(function (pet) {
+        return pet.id === parseInt(request.params.petId)
+    })
+    response.send(singlePet)
 })
 // POST /api/owners/:id/pets
 app.post('/api/owners/:id/pets', function (request, response, next) {
